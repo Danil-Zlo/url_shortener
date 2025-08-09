010f42f7aabad3d0ab6e00341a843ff0567cb4b5
maxround <dozlobin2002@mail.ru>
2025-08-09 17:41:59 +0300

    fix logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	slog "log/slog"
+	"os"
 
 	"github.com/Danil-Zlo/url_shortener/internal/config"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// var
+	if err := godotenv.Load("local.env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// init config: cleanenv
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
 
-	// TODO: init logger: slog
+	// init logger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
@@ -21,6 +34,23 @@ func main() {
 	// TODO: run server
 }
 
-func setupLogger(env string) {
-	//
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "dev":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	case "prod":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	}
+	return log
 }
