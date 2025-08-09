2dc223736b2b85b2000d3963ce8c654f60b5b94c
maxround <dozlobin2002@mail.ru>
2025-08-09 21:44:58 +0300

    add sqlite connection

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -7,18 +7,19 @@ import (
 	"os"
 
 	"github.com/Danil-Zlo/url_shortener/internal/config"
+	sl "github.com/Danil-Zlo/url_shortener/internal/lib/logger/slog"
+	"github.com/Danil-Zlo/url_shortener/internal/storage/sqlite"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	// var
+	// add enviroment var
 	if err := godotenv.Load("local.env"); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	// init config: cleanenv
 	cfg := config.MustLoad()
-
 	fmt.Println(cfg)
 
 	// init logger: slog
@@ -27,8 +28,15 @@ func main() {
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	// TODO: init storage: sqlite
+	// init storage: sqlite
 
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
